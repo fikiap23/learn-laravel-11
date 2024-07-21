e946287b6cfdd2e573e63becc226735c5c31b0d4
fikiaprian23 <fikiaprian23@gmail.com>
2024-07-21 20:44:23 +0700

    feat: faker, factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->paragraphs(5, true),
+            'author' => $this->faker->name(),
+            'slug' => Str::slug($this->faker->sentence()),
+        ];
+    }
+}
